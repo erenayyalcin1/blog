d64c64220d5cb8fc596382a9b889788bb52c0ea4
erenay yalcin <erenayyalcin@hotmail.com>
2022-06-09 03:40:24 +0300

    .,,.,.,,..,.,.,.,.,.,.,.,.

--- Login_BackEnd.js
+++ Login_BackEnd.js
@@ -4,60 +4,59 @@
 window.addEventListener("load", WebsiteLoaded);
 
 
-function WebsiteLoaded(){
-   
-    console.log("A1");
-    
-    document.getElementById("Hareketli_Giris").addEventListener("mouseenter",Ziplayiver);
 
+
+
+
+function WebsiteLoaded(){
+    document.getElementById("Hareketli_Giris").addEventListener("mouseenter",Ziplayiver);
+    document.getElementById("Hareketli_Giris1").addEventListener("mouseenter",Ziplayiver);
+   
 
 }
+
 function Ziplayiver(Element){
     var divim=Element.currentTarget;
     let a=Math.floor(Math.random()*4)+1;
-    let ass = divim.clientWidth;
+    let divGenisligi = divim.clientWidth;
 
 
     switch(a){
         case 1:
-            let yeniDeger =  (parseInt(divim.style.marginTop.replace("px", "")) + ass);
+            let yeniDeger =  (parseInt(divim.style.marginTop.replace("px", "")) + divGenisligi);
             if (yeniDeger >= window.innerHeight)
             {
-                yeniDeger = (parseInt(divim.style.marginTop.replace("px", "")) - ass);
+                yeniDeger = (parseInt(divim.style.marginTop.replace("px", "")) - divGenisligi);
             }
             divim.style.marginTop=yeniDeger  + 'px';
             break;
         case 2:
-            let yeniDeger2 =  (parseInt(divim.style.marginLeft.replace("px", "")) - ass);
+            let yeniDeger2 =  (parseInt(divim.style.marginLeft.replace("px", "")) - divGenisligi);
             if (yeniDeger2 < 0)
             {
-                yeniDeger2 =  (parseInt(divim.style.marginLeft.replace("px", "")) + ass);
+                yeniDeger2 =  (parseInt(divim.style.marginLeft.replace("px", "")) + divGenisligi);
             }
             divim.style.marginLeft=yeniDeger2  + 'px';
             break;
         case 3:
-            let yeniDeger3 =  (parseInt(divim.style.marginLeft.replace("px", "")) + ass);
+            let yeniDeger3 =  (parseInt(divim.style.marginLeft.replace("px", "")) + divGenisligi);
             if (yeniDeger3 >= window.innerWidth)
             {
-                yeniDeger3 =  (parseInt(divim.style.marginLeft.replace("px", "")) - ass);
+                yeniDeger3 =  (parseInt(divim.style.marginLeft.replace("px", "")) - divGenisligi);
             }
                 divim.style.marginLeft=yeniDeger3  + 'px';
                 break;  
         case 4:
-            let yeniDeger4 =  (parseInt(divim.style.marginTop.replace("px", "")) - ass);
+            let yeniDeger4 =  (parseInt(divim.style.marginTop.replace("px", "")) - divGenisligi);
             if (yeniDeger4 < 0)
             {
-                yeniDeger4 = (parseInt(divim.style.marginTop.replace("px", "")) + ass);
+                yeniDeger4 = (parseInt(divim.style.marginTop.replace("px", "")) + divGenisligi);
             }
             divim.style.marginTop= yeniDeger4  + 'px';
                 break;    
     }
 
 
-    console.log(
-        "Left: " + divim.style.marginLeft +
-        "Top: " + divim.style.marginTop 
-    );
 
 
 }
